9311d702b1fb2559a931844dd10d221e5c68a204
Jeff <vamay505@gmail.com>
2022-04-19 00:43:07 +0300

    displayed all movies when routed to each category

---
+++ src/app/categories/components/view-all/view-all.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { MovieCategoriesService } from '../../services/movie-categories/movie-categories.service';
+
+@Component({
+  selector: 'app-view-all',
+  templateUrl: './view-all.component.html',
+  styleUrls: ['./view-all.component.css'],
+})
+export class ViewAllComponent implements OnInit {
+  category: any;
+  categoriesData = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private categories: MovieCategoriesService
+  ) {}
+
+  ngOnInit(): void {
+    this.getCatalogue();
+  }
+
+
+  getCatalogue() {
+    this.route.paramMap.subscribe((params) => {
+      this.category = params.get('category');
+      this.categories.getCategories(this.category).subscribe(resp => {
+        this.categoriesData = resp.items;
+        console.log('wahh', resp.items);
+      });
+    });
+  }
+}
+
+
+  // fetchCatalogue() {
+  //   this.route.paramMap.subscribe((params) => {
+  //     this.categories.getCategories(this.category).subscribe(resp => {
+  //       this.movieList =resp.items;
+  //       console.log('catalog', resp.items);
+  //     })
+  //   })
+ 
+  // }
