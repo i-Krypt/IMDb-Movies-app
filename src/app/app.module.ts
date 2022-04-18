cf697f7c52798fd393187cd50ef1838f5ce0652f
Jeff <vamay505@gmail.com>
2022-04-19 00:34:13 +0300

    setting up the section with nav and bottom nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
-import { CategoriesModule } from './categories/categories.module'
+import { CategoriesModule } from './categories/categories.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { CategoriesModule } from './categories/categories.module'
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
