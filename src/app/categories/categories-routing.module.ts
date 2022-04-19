efca318075adf40e3ce01543ed9fa6bb8f43f8fc
Jeff <vamay505@gmail.com>
2022-04-20 00:32:33 +0300

    added a route for movie details

--- src/app/categories/categories-routing.module.ts
+++ src/app/categories/categories-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ComingSoonComponent } from './components/coming-soon/coming-soon.component';
 import { ViewAllComponent } from './components/view-all/view-all.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 
 
 const routes: Routes = [
   { path: '',  component: ComingSoonComponent },
   { path: 'category/:category',  component: ViewAllComponent },
+  { path: 'movie-details/:id',  component: ViewAllComponent }
 ];
 
 @NgModule({
